3bbd44625f20cc5913c437b1d7619e673d53da3f
AkiraSerenity <luminost87@gmail.com>
2026-02-21 21:08:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,84 +4,84 @@
         { 
             name: 'Gery', 
             username: '@gerysenwith_lapiskeju', 
-            tiktok: 'https://www.tiktok.com/@gerysenwith_lapiskeju', 
+            tiktok: 'https://www.tiktok.com/@gerysenwith_lapiskeju?_r=1&_t=ZS-9418k7vyLUi'', 
             photo: 'images/member1.jpg', 
-            quote: 'Kamu rimuru bukan?'
+            quote: 'Bahkan hal sederhana seperti azan magrib bisa menjadi sepesial hijau engkau menantikan nya'
         },
         { 
             name: 'Rora', 
             username: '@roraxml', 
-            tiktok: 'https://www.tiktok.com/@kasugano_sora87', 
+            tiktok: 'https://www.tiktok.com/@kasugano_sora87?_r=1&_t=ZS-9418UBnTGEe', 
             photo: 'images/member2.jpeg', 
-            quote: 'Aku mah raja'
+            quote: 'Cerita yg singkat terkadang membawa luka yang hebat'
         },
         { 
             name: 'Izabella', 
             username: '@queen.izabellaz', 
-            tiktok: 'https://www.tiktok.com/@queen.izabellaz', 
+            tiktok: 'https://www.tiktok.com/@queen.izabellaz?_r=1&_t=ZS-9418I1azkJA', 
             photo: 'images/member3.jpeg', 
-            quote: 'Mending turu'
+            quote: 'Manusia tidak bisa merubah waktu, tapi dengan berjalan nya waktu manusia bisa berubah'
         },
         { 
             name: 'Erie', 
             username: '@kasugano_erie', 
-            tiktok: 'https://www.tiktok.com/@kasugano_erie', 
+            tiktok: 'https://www.tiktok.com/@kasugano_erie?_r=1&_t=ZS-9417ozfwDFU', 
             photo: 'images/member4.jpg', 
             quote: 'Gw butuh uang!!!'
         },
         { 
             name: 'Fuyi', 
             username: '@fuyii_npc', 
-            tiktok: 'https://www.tiktok.com/@fuyii_npc', 
+            tiktok: 'https://www.tiktok.com/@fuyii_npc?_r=1&_t=ZS-9417m1YFkM1', 
             photo: 'images/member5.png', 
             quote: 'Belajar untuk memahami diri kita sendiri, terkadang kita tidak harus memahami seseorang, bahkan juga orang belum bisa memahami kita sepenuhnya, maka belajar lah untuk menerima kenyataan'
         },
         { 
             name: 'Ezan', 
             username: '@ezan_xd', 
-            tiktok: 'https://www.tiktok.com/@ezan_xd', 
+            tiktok: 'https://www.tiktok.com/@ezan_xd?_r=1&_t=ZS-9417lftdDdN', 
             photo: 'images/member6.png', 
-            quote: 'Sederhana tapi berarti, itulah keluarga.'
+            quote: 'Seorang pria dengan sebuah tombak adalah sebuah benteng'
         },
         { 
             name: 'V3iin', 
             username: '@iyainirhmd', 
-            tiktok: 'https://www.tiktok.com/@iyainirhmd', 
+            tiktok: 'https://www.tiktok.com/@iyainirhmd?_r=1&_t=ZS-9417kmmsdHA', 
             photo: 'images/member7.jpg', 
             quote: 'bntr'
         },
         { 
             name: 'Shaglen', 
             username: '@xmj.shaglen', 
-            tiktok: 'https://www.tiktok.com/@xmj.shaglen', 
+            tiktok: 'https://www.tiktok.com/@xmj.shaglen?_r=1&_t=ZS-9417Zfr2QU8', 
             photo: 'images/member8.jpg', 
             quote: 'Id have'
         },
         { 
             name: 'Valen', 
             username: '@naineyuko', 
-            tiktok: 'https://www.tiktok.com/@naineyuko', 
+            tiktok: 'https://www.tiktok.com/@naineyuko?_r=1&_t=ZS-9417icJsZ5I', 
             photo: 'images/member9.jpg', 
-            quote: 'Aduh kok aku jadi cewek siehh'
+            quote: 'Hidup itu pilihan so just gambling brooo'
         },
         { 
             name: 'Yuuta', 
             username: '@yuutaxml_', 
-            tiktok: 'https://www.tiktok.com/@yuutaxml_', 
+            tiktok: 'https://www.tiktok.com/@yuutaxml_?_r=1&_t=ZS-9417ceeQYFw', 
             photo: 'images/member10.jpg', 
             quote: 'You never know until you try'
         },
         { 
             name: 'Jarzz', 
             username: '@jarzz.dw.skuy', 
-            tiktok: 'https://www.tiktok.com/@jarzz.dw.skuy', 
+            tiktok: 'https://www.tiktok.com/@jarzz.dw.skuy?_r=1&_t=ZS-9417iM9qpN6', 
             photo: 'images/member11.jpg', 
             quote: 'Laparr'
         },
         { 
-            name: 'Qwentty', 
-            username: '@qwentty.ft', 
-            tiktok: 'https://www.tiktok.com/@qwenty.ft', 
+            name: 'Qwenty', 
+            username: '@qwenty.ft', 
+            tiktok: 'https://www.tiktok.com/@qwenty.ft?_r=1&_t=ZS-9417dPeBf4m'', 
             photo: 'images/member12.jpg', 
             quote: 'Semua manusia hanyalah alat'
         },
@@ -90,35 +90,35 @@
             username: '@akira.imut', 
             tiktok: 'https://www.tiktok.com/@akiraa.imut', 
             photo: 'images/member13.jpg', 
-            quote: 'Imut gini kalo bantai lu sabi kali'
+            quote: 'Lahhh, gw mah jago'
         },
         { 
             name: 'Dree', 
             username: '@daktau_256', 
-            tiktok: 'https://www.tiktok.com/@daktau_256', 
+            tiktok: 'https://www.tiktok.com/@daktau_256?_r=1&_t=ZS-9417gOvhoKy'', 
             photo: 'images/member14.jpg', 
-            quote: 'Jangan takut salah, karena dari salah kita belajar.'
+            quote: 'AFK njirrr orang nya.'
         },
         { 
             name: 'Zunda', 
             username: '@shaglenv02y', 
-            tiktok: 'https://www.tiktok.com/@shaglenv02y', 
+            tiktok: 'https://www.tiktok.com/@shaglenv02y?_r=1&_t=ZS-9417etBXVki', 
             photo: 'images/member15.jpg', 
-            quote: 'Emel yok'
+            quote: 'Kalo kamu lapar makan lah'
         },
         { 
             name: 'Yushaa Von Khaslana', 
             username: '@kachusyaa', 
-            tiktok: 'https://www.tiktok.com/@kachusyaa', 
+            tiktok: 'https://www.tiktok.com/@kachusyaa?_r=1&_t=ZS-943UkIGrPbZ', 
             photo: 'images/member16.jpeg', 
             quote: 'Jangan lupa makan'
         },
         { 
-            name: 'Yushaa Von Khaslana', 
-            username: '@kachusyaa', 
-            tiktok: 'https://www.tiktok.com/@kachusyaa', 
-            photo: 'images/member16.jpeg', 
-            quote: 'Jangan lupa makan'
+            name: 'Zakk', 
+            username: '@sb_prst', 
+            tiktok: 'https://www.tiktok.com/@sb_prst?_r=1&_t=ZS-946jrKwcwxo', 
+            photo: 'images/member17.jpeg', 
+            quote: 'Jangan lupa main epep'
         }
     ];
 
@@ -505,3 +505,4 @@
     });
 
 })();
+
