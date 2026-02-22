8b6a2b80bea2f6c760c6c33465d182b98c0a7afa
AkiraSerenity <luminost87@gmail.com>
2026-02-22 22:14:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 (function() {
     const members = [
-        { name: 'Gery', username: '@gerysenwith_lapiskeju', tiktok: 'https://www.tiktok.com/@gerysenwith_lapiskeju', photo: 'images/member1.jpg', quote: 'Kamu rimuru bukan?' },
-        { name: 'Rora', username: '@roraxml', tiktok: 'https://www.tiktok.com/@kasugano_sora87', photo: 'images/member2.jpeg', quote: 'Aku mah raja' },
-        { name: 'Izabella', username: '@queen.izabella', tiktok: 'https://www.tiktok.com/@queen.izabella', photo: 'images/member3.jpeg', quote: 'Mending turu' },
+        { name: 'Gery', username: '@gerysenwith_lapiskeju', tiktok: 'https://www.tiktok.com/@gerysenwith_lapiskeju', photo: 'images/member1.jpg', quote: 'Bahkan hal sederhana seperti azan magrib bisa menjadi sepesial hijau engkau menantikan nya' },
+        { name: 'Rora', username: '@roraxml', tiktok: 'https://www.tiktok.com/@kasugano_sora87', photo: 'images/member2.jpeg', quote: 'Pertemuan terkadang memberikan luka yang sangat dalam jika perpisahan telah tiba' },
+        { name: 'Izabella', username: '@queen.izabella', tiktok: 'https://www.tiktok.com/@queen.izabella', photo: 'images/member3.jpeg', quote: 'Janji manusia? Itu hanyalah pemanis, manusia lebih sering mengingkari dari pada menepati' },
         { name: 'Erie', username: '@kasugano_erie', tiktok: 'https://www.tiktok.com/@kasugano_erie', photo: 'images/member4.jpg', quote: 'Gw butuh uang!!!' },
-        { name: 'Fuyi', username: '@fuyi_npc', tiktok: 'https://www.tiktok.com/@fuyi_npc', photo: 'images/member5.png', quote: 'Belajar untuk memahami diri kita sendiri' },
+        { name: 'Fuyi', username: '@fuyi_npc', tiktok: 'https://www.tiktok.com/@fuyi_npc', photo: 'images/member5.png', quote: 'Belajar untuk memahami diri kira sendiri, terkadang kita tidak harus memahami seseorang, bahkan juga orang belum bisa memahami kita sepenuhnya,maka belajar lah untuk menerima kenyataan' },
         { name: 'V3iin', username: '@iyainirhmd', tiktok: 'https://www.tiktok.com/@iyainirhmd', photo: 'images/member7.jpg', quote: 'bntr' },
         { name: 'Yuta', username: '@yutaxml', tiktok: 'https://www.tiktok.com/@yutaxml', photo: 'images/member10.jpg', quote: 'You never know until you try' },
         { name: 'Jarzz', username: '@jarzz.dw.skuy', tiktok: 'https://www.tiktok.com/@jarzz.dw.skuy', photo: 'images/member11.jpg', quote: 'Laparr' },
-        { name: 'Akira', username: '@akira.imut', tiktok: 'https://www.tiktok.com/@akiraa.imut', photo: 'images/member13.jpg', quote: 'Imut gini kalo bantai lu sabi kali' },
+        { name: 'Akira', username: '@akira.imut', tiktok: 'https://www.tiktok.com/@akiraa.imut', photo: 'images/member13.jpg', quote: 'Lahhhh, gw mah jago' },
         { name: 'Dree', username: '@daktau_256', tiktok: 'https://www.tiktok.com/@daktau_256', photo: 'images/member14.jpg', quote: 'Jangan takut salah, karena dari salah kita belajar.' },
-        { name: 'Zunda', username: '@shaglenv02y', tiktok: 'https://www.tiktok.com/@shaglenv02y', photo: 'images/member15.jpg', quote: 'Emel yok' },
+        { name: 'Zunda', username: '@shaglenv02y', tiktok: 'https://www.tiktok.com/@shaglenv02y', photo: 'images/member15.jpg', quote: 'Kalo kamu lapar makan lahh' },
         { name: 'Yushaa Von Khaslana', username: '@kachusyaa', tiktok: 'https://www.tiktok.com/@kachusyaa?_r=1&_t=ZS-943UkIGrPbZ', photo: 'images/member16.jpeg', quote: 'Jangan lupa makan' },
-        { name: 'Zakk', username: '@sb_prst', tiktok: 'https://www.tiktok.com/@sb_prst?_r=1&_t=ZS-948PdFDTDAj', photo: 'images/member17.jpeg', quote: 'Jangan lupa main epep' }
+        { name: 'Zakk', username: '@sb_prst', tiktok: 'https://www.tiktok.com/@sb_prst?_r=1&_t=ZS-948PdFDTDAj', photo: 'images/member17.jpeg', quote: 'Jangan lupa main epep' },
+        { name: 'Shaglen', username: '@xmj.shaglen', tiktok: 'https://www.tiktok.com/@xmj.shaglen?_r=1&_t=ZS-948UjlCNgvs', photo: 'images/member8.jpeg', quote: 'Id have' }
     ];
 
     const admins = [
@@ -30,7 +31,7 @@
         { platform: 'Instagram', username: '@kasugano.family', link: 'https://www.instagram.com/kasugano.family', icon: 'fab fa-instagram' },
         { platform: 'Instagram', username: '@kasuganostore', link: 'https://www.instagram.com/kasuganostore', icon: 'fab fa-instagram' },
         { platform: 'WhatsApp', username: '+62 852-3240-6495', link: 'https://wa.me/6285232406495', icon: 'fab fa-whatsapp' },
-        { platform: 'Email', username: 'kasuganofamilyy@gmail.com', link: 'mailto:kasuganofamilyy@gmail.com', icon: 'far fa-envelope' }
+        { platform: 'Email', username: 'kasuganofamilyy@gmail.com', link: 'kasuganofamilyy@gmail.com', icon: 'far fa-envelope' }
     ];
 
     const navbar = document.getElementById('navbar');
@@ -281,3 +282,4 @@
     window.addEventListener('load', () => setTimeout(updateScrollPadding, 200));
 
 })();
+
